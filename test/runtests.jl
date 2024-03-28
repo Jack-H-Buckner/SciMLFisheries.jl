97ea24a06ae9a75d5a3ca8b79ee050be15d4a4a8
Jack Buckner <Jhbuckner@ucdavis.edu>
2024-03-28 10:49:37 -0400

    added example code in docs to runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "SciMLFisheries.jl" begin
     include("test_surplus_production.jl")
+    include("test_docs_examples.jl")
 end
--- test/test_docs_examples.jl
+++ test/test_docs_examples.jl
@@ -8,4 +8,8 @@ model = SurplusProduction(data,production_model = "LSTM", produciton_hyper_param
 model = SurplusProduction(data,production_model = "LSTMDropOut", produciton_hyper_parameters = (cell_dim = 10, drop_prob = 0.1))
 model = SurplusProduction(data,production_model = "FeedForward",produciton_hyper_parameters = (hidden=10,seed=123,extrap_value=0.0,extrap_length=0.5),regularizaiton_type = "L2", regularizaiton_weight = 10^-4 )
 
-# 
+# Observation models
+model = SurplusProduction(data,harvest_model = "DiscreteAprox",theta = 1.0)
+model = SurplusProduction(data,harvest_model = "LinearAprox")
+model = SurplusProduction(data, index_model="Linear",prior_q = 0.0,prior_weight = 0.0)
+model = SurplusProduction(data,index_model="HyperStability",prior_q = 0.0,prior_b = 1.0,prior_weight = (q = 0.0, b = 0.0))
